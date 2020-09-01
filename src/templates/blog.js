8231c2808d626244df084c0a24ee2239a8d6b631
Beros <59673191+Panosmpero@users.noreply.github.com>
2020-09-01 10:24:06 +0100

    extract text-img from contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,25 +1,49 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+// export const query = graphql`
+//   query($slug: String) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
 
 export const query = graphql`
   query($slug: String) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "Do MMMM YYYY")
+      body {
+        json
       }
-      html
     }
   }
 `
 
-const Blog = ({data}) => {
-  return <Layout>
-    <h1>{data.markdownRemark.frontmatter.title}</h1>
-    <p>{data.markdownRemark.frontmatter.date}</p>
-    <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
-  </Layout>
+const Blog = ({ data }) => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        const src = node.data.target.fields.file["en-US"].url
+        const alt = node.data.target.fields.title["en-US"]
+        return <img src={src} alt={alt}/>
+      }
+    }
+  }
+  return (
+    <Layout>
+      <h1>{data.contentfulBlogPost.title}</h1>
+      <p>{data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(data.contentfulBlogPost.body.json, options)}
+    </Layout>
+  )
 }
 
 export default Blog
